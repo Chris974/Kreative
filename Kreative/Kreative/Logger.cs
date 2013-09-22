09d6dd57754b6254081dd80d10891d10f53cb25b
Chris974 <chris974@live.com>
2013-09-22 13:59:00 -0400

    Inital commit
    Just a quick start to the source.

---
+++ Kreative/Kreative/Logger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kreative
+{
+    class Logger
+    {
+        public static void Log(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+        }
+    }
+}
---
+++ Kreative/Kreative/Server.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Kreative
+{
+    public class Server
+    {
+        public TcpListener listener;
+        public Server s;
+        private Properties p { get; set; }
+        public bool isRunning;
+
+        private Logger logger { get; set; }
+
+        public Server()
+        {
+
+        }
+
+        public void StartServer()
+        {
+            try
+            {
+                if (s.isRunning != true)
+                {
+                    Console.Title = "KreativeSMP: V1.0.0";
+                    Logger.Log("[Kreative]: Starting server...");
+                    listener = new TcpListener(IPAddress.Any, p.port);
+                    listener.Start();
+                    while (true)
+                    {
+                        Console.ReadLine();
+                    }
+                }
+            }
+            catch
+            {
+            }
+        }
+    }
+}
